ed294fbd0eaed9bd5c0842e31166dd85b21eb5c9
emanuellzoe <emanuellzoee@gmail.com>
2025-11-10 13:45:14 +0700

    commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,13 +7,74 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css">
-    
-    <title>Document</title>
+    <style>
+    /* Buat footer selalu di bawah */
+    html, body {
+      height: 100%;
+      margin: 0;
+    }
+    body {
+      display: flex;
+      flex-direction: column;
+      min-height: 100vh;
+    }
+    main {
+      flex: 1;
+    }
+  </style>
+
+    <title> </title>
 </head>
 <body>
+    <!-- NAVBAR (atas) -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <!-- Jadikan iPhoneStore juga sebagai tombol Home -->
+      <a class="navbar-brand" href="{{ url('/') }}">iPhoneStore</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMain">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navMain">
+        <ul class="navbar-nav mr-auto">
+          <!-- hanya Products dan Add Product -->
+          <li class="nav-item"><a class="nav-link" href="{{ url('/product') }}">Products</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ url('/product/addform') }}">Add Product</a></li>
+        </ul>
+
+        <!-- kanan: cart dan user dropdown -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="bi bi-cart3"></i> Cart <span class="badge badge-pill badge-light">0</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="userMenu" data-toggle="dropdown" href="#">User</a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="#">Profile</a>
+              <a class="dropdown-item" href="#">Logout</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- MAIN CONTENT -->
+  <main class="container-fluid mt-4">
+    <div class="card shadow-sm">
+      <div class="card-body">
+        @yield('content')
+      </div>
+    </div>
+  </main>
+
+  <!-- FOOTER -->
+  <footer class="bg-dark text-white text-center py-3 mt-auto">
+    &copy; {{ date('Y') }} iPhoneStore. All rights reserved.
+  </footer>
+
     
-
-
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
