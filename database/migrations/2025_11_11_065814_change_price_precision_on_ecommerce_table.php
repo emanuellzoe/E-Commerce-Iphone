a0a26f275b1c921f969a01e034b71f7af6d395d6
emanuellzoe <emanuellzoee@gmail.com>
2025-11-11 14:24:55 +0700

    commit

--- database/migrations/2025_11_11_065814_change_price_precision_on_ecommerce_table.php
+++ database/migrations/2025_11_11_065814_change_price_precision_on_ecommerce_table.php
@@ -6,27 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class ChangePricePrecisionOnEcommerceTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('ecommerce', function (Blueprint $table) {
-            //
+            // ubah precision menjadi 15,2 (nilai besar dan 2 desimal)
+            $table->decimal('price', 15, 2)->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('ecommerce', function (Blueprint $table) {
-            //
+            // kembalikan bila perlu (sesuaikan dengan nilai awal, misal 8,2)
+            $table->decimal('price', 8, 2)->change();
         });
     }
 }
