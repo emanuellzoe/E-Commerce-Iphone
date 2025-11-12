3cc8899ce58eccb8b4895a41fc5fe48cff125bf0
emanuellzoe <emanuellzoee@gmail.com>
2025-11-12 21:28:40 +0700

    put dan value pada produuctEdit.blade

--- resources/views/productEdit.blade.php
+++ resources/views/productEdit.blade.php
@@ -7,9 +7,12 @@
   <h3 class="mb-4 text-light">Tambah Produk Baru</h3>
   <form action="{{ url('/product/save') }}" method="POST" enctype="multipart/form-data">
     @csrf
+    @method('PUT')
+
+
     <div class="form-group">
       <label for="product_name">Nama Produk</label>
-      <input type="text" name="product_name" id="product_name" class="form-control" required>
+      <input type="text" name="product_name" id="product_name" class="form-control" value="{{ $product->product_name }}">
     </div>
     <div class="form-group">
       <label for="description">Deskripsi</label>
