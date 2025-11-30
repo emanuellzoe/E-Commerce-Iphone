d025fefb64a6919bb1ddb97ca7d358380656aeed
emanuellzoe <emanuellzoee@gmail.com>
2025-11-30 19:56:11 +0700

    commit

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -8,7 +8,7 @@
   <!-- pop up -->
   @if (session('alert'))
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{{ session(key: 'alert') }}</strong>
+        <strong>{{ session('alert') }}</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
